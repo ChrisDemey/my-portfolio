98088582ca8fa0ec9a9b28c789b8c9f015c19a1f
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2023-06-06 17:34:07 +0200

    🐛 Fixed a bug that shifted container height on mobile

--- components/Container.vue
+++ components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute z-10 h-full w-full">
+    <div class="absolute z-10 h-screen w-full">
         <div class="h-2/3 px-4 md:px-0 pb-4 mx-auto flex items-end justify-center max-w-4xl">
             <slot name="top"></slot>
         </div>
