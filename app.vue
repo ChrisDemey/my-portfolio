25065a59aa217dce86255781f49b04c431ab611a
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2024-09-29 15:56:55 +0200

    :sparkles: Feat: Added something behind everything

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
 <template>
     <NuxtLayout />
 </template>
+
+<script setup lang="ts">
+console.log("%c.",
+    "margin-top: 20px;" +
+    "margin-bottom: 20px;" +
+    "padding-left: 20px;" +
+    "padding-right: 20px;" +
+    "padding-top: 16px;" +
+    "padding-bottom: 16px;" +
+    "border-radius: 9999px;" +
+    "color: transparent;" +
+    "background: linear-gradient(to top, #14b8a6 1.5%, #0000 2%, #0000 5%, #14b8a6 5.5%, #14b8a6 7.5%, #0000 8%, #0000 10.6%, #14b8a6 11.1%, #14b8a6 13.6%, #0000 14.1%, #0000 16.3%, #14b8a6 16.8%, #14b8a6 19.8%, #0000 20.3%, #0000 22.1%, #14b8a6 22.6%, #14b8a6 26.1%, #0000 26.6%, #0000 28%, #14b8a6 28.5%, #14b8a6 32.5%, #0000 33%, #0000 34%, #14b8a6 34.5%, #14b8a6 39%, #0000 39.5%, #0000 40.1%, #14b8a6 40.6%, #ec4899 75%) no-repeat;")
+</script>
