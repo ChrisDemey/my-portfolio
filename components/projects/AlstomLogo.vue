f3e18bd67ce377aab65ce78105f6d3bde1a9b330
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2024-12-28 12:54:31 +0100

    🎨 style(projects page): Added Alstom logo

---
+++ components/projects/AlstomLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    style="--color: rgb(94 234 212)"
+    class="flex items-center justify-center w-full mb-2"
+  >
+    <img
+      class="rounded-xl h-28 bg-white/50 border-2 border-[--color] shadow-neon-inner"
+      src="/images/projects/Alstom_logo.png"
+      alt="Alstom logo"
+    />
+  </div>
+</template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,83 +1,86 @@
 <template>
-    <Head>
-        <Title>&#60;Portfolio :page="Projects" &#47;&#62;</Title>
-        <Meta name="Projects page" content="All of my client projects"/>
-    </Head>
+  <Head>
+    <Title>&#60;Portfolio :page="Projects" &#47;&#62;</Title>
+    <Meta name="Projects page" content="All of my client projects" />
+  </Head>
 
-    <Modal
-        v-if="showCarsharingSwiper"
-        @close="showCarsharingSwiper = false"
-    >
-        <Carousel
-            :images="carsharingImages"
+  <Modal v-if="showCarsharingSwiper" @close="showCarsharingSwiper = false">
+    <Carousel :images="carsharingImages" />
+  </Modal>
+
+  <Modal v-if="showDesksharingSwiper" @close="showDesksharingSwiper = false">
+    <Carousel :images="desksharingImages" />
+  </Modal>
+
+  <div class="absolute z-10 w-full h-screen md:hidden">
+    <ul class="flex items-end h-screen px-6 pb-6 space-x-10 overflow-y-auto">
+      <li v-for="(item, index) in projects" :key="index" class="space-y-4">
+        <ImagesCard
+          :name="item.name"
+          :image="item.image"
+          @click="
+            item.name === 'carsharing'
+              ? (showCarsharingSwiper = true)
+              : (showDesksharingSwiper = true)
+          "
         />
-    </Modal>
+        <DescriptionCard :text="item.text" />
+      </li>
+    </ul>
+  </div>
 
-    <Modal
-        v-if="showDesksharingSwiper"
-        @close="showDesksharingSwiper = false"
-    >
-        <Carousel
-            :images="desksharingImages"
-        />
-    </Modal>
-
-    <div class="absolute z-10 w-full h-screen md:hidden">
-        <ul class="flex items-end h-screen px-6 pb-6 space-x-10 overflow-y-auto">
-            <li v-for="(item, index) in projects" :key="index" class="space-y-4">
-                <ImagesCard
-                    :name="item.name"
-                    :image="item.image"
-                    @click="item.name === 'carsharing' ? showCarsharingSwiper = true : showDesksharingSwiper = true"
-                />
-                <DescriptionCard
-                    :text="item.text"
-                />
-            </li>
-        </ul>
+  <div class="absolute z-10 hidden w-full h-screen md:block">
+    <div class="flex flex-col justify-end pb-4 h-2/3">
+      <alstom-logo />
+      <ul class="flex items-end justify-center space-x-6">
+        <li v-for="(item, index) in projects" :key="index" class="space-y-4">
+          <ImagesCard
+            :name="item.name"
+            :image="item.image"
+            @click="
+              item.name === 'carsharing'
+                ? (showCarsharingSwiper = true)
+                : (showDesksharingSwiper = true)
+            "
+          />
+        </li>
+      </ul>
     </div>
-
-    <div class="absolute z-10 hidden w-full h-screen md:block">
-        <ul class="flex items-end justify-center pb-4 space-x-6 h-2/3">
-            <li v-for="(item, index) in projects" :key="index" class="space-y-4">
-            <ImagesCard
-                :name="item.name"
-                :image="item.image"
-                @click="item.name === 'carsharing' ? showCarsharingSwiper = true : showDesksharingSwiper = true"
-            />
-            </li>
-        </ul>
-        <ul class="flex justify-center pt-4 space-x-6 h-1/3">
-            <li v-for="(item, index) in projects" :key="index">
-            <DescriptionCard
-                :text="item.text"
-            />
-            </li>
-        </ul>
-    </div>
-
+    <ul class="flex justify-center pt-4 space-x-6 h-1/3">
+      <li v-for="(item, index) in projects" :key="index">
+        <DescriptionCard :text="item.text" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-const showCarsharingSwiper = ref(false)
-const showDesksharingSwiper = ref(false)
+const showCarsharingSwiper = ref(false);
+const showDesksharingSwiper = ref(false);
 
 const projects = [
-    { name:'carsharing', image:'/images/projects/carsharing/trips_list.png', text: 'carsharing_description' },
-    { name:'desksharing', image:'/images/projects/desksharing/devices_filter.png', text: 'desksharing_description' },
-]
+  {
+    name: "carsharing",
+    image: "/images/projects/carsharing/trips_list.png",
+    text: "carsharing_description",
+  },
+  {
+    name: "desksharing",
+    image: "/images/projects/desksharing/devices_filter.png",
+    text: "desksharing_description",
+  },
+];
 
 const carsharingImages = [
-    { image:'/images/projects/carsharing/trips_list.png' },
-    { image:'/images/projects/carsharing/trip_details.png' },
-    { image:'/images/projects/carsharing/my_vehicle.png' },
-    { image:'/images/projects/carsharing/my_places.png' },
-    { image:'/images/projects/carsharing/creating_trip.png' },
-]
+  { image: "/images/projects/carsharing/trips_list.png" },
+  { image: "/images/projects/carsharing/trip_details.png" },
+  { image: "/images/projects/carsharing/my_vehicle.png" },
+  { image: "/images/projects/carsharing/my_places.png" },
+  { image: "/images/projects/carsharing/creating_trip.png" },
+];
 
 const desksharingImages = [
-    { image:'/images/projects/desksharing/devices_filter.png' },
-    { image:'/images/projects/desksharing/settings_page.png' },
-
-]
+  { image: "/images/projects/desksharing/devices_filter.png" },
+  { image: "/images/projects/desksharing/settings_page.png" },
+];
 </script>
